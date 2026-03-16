4b5db79d0c96ab86ae0c73160e17e3d21dffadfe
刘宇辉 <@lyh>
2026-03-16 08:44:32 +0800

    统一element 主题色

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,6 +6,7 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import App from './App.vue'
 import router from './router'
+import './styles/themes.css'
 import './styles/index.css'
 import { useAuthStore } from './stores/auth'
 
